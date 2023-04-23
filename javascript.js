a15e6fde64cbd4c55bf06be489481475193f4e81
julinhodoceu <julio06.sp@gmail.com>
2023-04-23 15:41:18 -0300

    atualização exercicio

--- javascript.js
+++ javascript.js
@@ -17,11 +17,14 @@ for (var contador = 0; contador < pacientes.length; contador++) {
     if (peso < 0 || peso > 600) {
         pesoEhValido = false
         imcinfo.textContent = 'Peso inválido'
+        pacientes[contador].classList.add('erro')
+
     }
 
     if (altura < 0 || altura > 3) {
         alturaEhValida = false
         imcinfo.textContent = 'Altura inválida'
+        pacientes[contador].classList.add('erro')
     }
 
     if (pesoEhValido == false && alturaEhValida == false) {
@@ -29,9 +32,42 @@ for (var contador = 0; contador < pacientes.length; contador++) {
     }
 
     if (pesoEhValido && alturaEhValida) {
-        imcinfo.textContent = Number(imc.toFixed(2))
+        imcinfo.textContent = imc.toFixed(2)
     }
 
 }
 
+var botãoAdicionar = document.querySelector('#adicionar-paciente')
+
+botãoAdicionar.addEventListener ('click', function() {
+    event.preventDefault();
+    var form = document.querySelector('#form-adiciona');
+    var nome = form.nome.value;
+    var altura = form.altura.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement('tr');
+    
+    var nomeTd = document.createElement('td');
+    var pesoTd = document.createElement('td');
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+    var imcTd = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+   
+
+    var tabelapacientes = document.querySelector('#tabela-pacientes')
+    
+    tabelapacientes.appendChild(pacienteTr)
+})
 
